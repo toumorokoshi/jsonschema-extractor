29411ba29c70b469c2574937b06f4204994739a0
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2017-06-15 20:55:56 -0700

    initial commit.

---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import glob
+import os
+import sys
+from setuptools import setup, find_packages
+
+is_release = False
+if "--release" in sys.argv:
+    is_release = True
+    sys.argv.remove("--release")
+
+base = os.path.dirname(os.path.abspath(__file__))
+
+README_PATH = os.path.join(base, "README.rst")
+
+install_requires = [
+    'attrs',
+    'schematics>=2.0.0',
+    'six',
+    'jinja2',
+    'swagger-schema>=0.4.0',
+    'pyyaml',
+]
+
+tests_require = []
+
+setup(name='attrs-jsonschema',
+      setup_requires=["vcver"],
+      vcver={
+          "is_release": is_release,
+          "path": base
+      },
+      description=(
+          "a utility library to help provide api route "
+          "generation form function signature for web "
+          "frameworks."
+      ),
+      long_description=open(README_PATH).read(),
+      author='Yusuke Tsutsumi',
+      author_email='yusuke@tsutsumi.io',
+      url='https://github.com/toumorokoshi/attrs-jsonschema',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=install_requires,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Operating System :: MacOS',
+          'Operating System :: POSIX :: Linux',
+          'Topic :: System :: Software Distribution',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+      ],
+      tests_require=tests_require
+)
---
+++ ubuild.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+import subprocess
+import uranium
+from uranium.rules import rule, Once
+
+
+def main(build):
+    build.packages.install(".", develop=True)
+
+
+@uranium.task_requires("main")
+def test(build):
+    """ execute the unit tests. """
+    build.packages.install("pytest")
+    build.packages.install("pytest-benchmark")
+    build.packages.install("pytest-cov")
+    build.packages.install("flake8")
+    build.executables.run([
+        "py.test", "--cov", "attrs_jsonschema",
+        "attrs_jsonschema/tests",
+        "--cov-report", "term-missing"
+    ] + build.options.args)
+
+
+def publish(build):
+    """ publish the package itself """
+    build.packages.install("wheel")
+    build.executables.run([
+        "python", "setup.py",
+        "sdist", "bdist_wheel", "--universal", "upload", "--release"
+    ])
+
+
+def build_docs(build):
+    changelog(build)
+    build.packages.install("Babel")
+    build.packages.install("Sphinx")
+    build.packages.install("sphinx_rtd_theme")
+    build.packages.install("sphinxcontrib-programoutput")
+    return build.executables.run([
+        "sphinx-build", "docs",
+        os.path.join("docs", "_build")
+    ] + build.options.args)[0]
