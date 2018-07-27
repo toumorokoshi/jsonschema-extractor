aeb1ad011ac00c1dffae08e025f064f6a47580e7
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-07-26 23:38:51 -0700

    adding python3.7 support

--- jsonschema_extractor/tests/test_typing.py
+++ jsonschema_extractor/tests/test_typing.py
@@ -2,10 +2,13 @@ import pytest
 from datetime import datetime
 from typing import Any, List, Set
 from jsonschema_extractor.typing_extractor import TypingExtractor
+from jsonschema_extractor.typing_extractor import PEP_560
+
 
 @pytest.fixture
 def typing_extractor():
     return TypingExtractor()
+
 
 @pytest.mark.parametrize("inp, expected_output", [
     (int, {"type": "integer"}),
@@ -24,6 +27,7 @@ def test_extract_typing(extractor, inp, expected_output):
     assert extractor.extract(inp) == expected_output
 
 
+@pytest.mark.skipif(PEP_560, reason="sets are not PEP_560 compatible")
 def test_typing_extractor_register(typing_extractor):
 
     def extract_set(extractor, typ):
@@ -42,4 +46,21 @@ def test_typing_extractor_register(typing_extractor):
         "type": "array",
         "title": "set",
         "items": {"type": "integer"}
+    }
+
+
+def test_typing_extractor_register(typing_extractor):
+
+    class Foo(object):
+        pass
+
+    def extract_foo(extractor, typ):
+        return {
+            "type": "string",
+        }
+
+    typing_extractor.register(Foo, extract_foo)
+
+    assert typing_extractor.extract(typing_extractor, Foo) == {
+        "type": "string",
     }
--- jsonschema_extractor/typing_extractor.py
+++ jsonschema_extractor/typing_extractor.py
@@ -1,8 +1,12 @@
+import sys
 from datetime import datetime
 from .compat import string_type
 from typing import (
     Any, List, Sequence
 )
+PEP_560 = sys.version_info[:3] >= (3, 7, 0)
+if PEP_560:
+    from typing import _GenericAlias
 
 
 class TypingExtractor(object):
@@ -10,7 +14,8 @@ class TypingExtractor(object):
     def __init__(self):
         self._extractor_list = []
         self._extractor_list.append((string_type, _extract_string))
-        self._extractor_list.append((List, _extract_seq))
+        if PEP_560:
+            self._extractor_list.append((List, _extract_seq))
         self._extractor_list.append((Sequence, _extract_seq))
         self._extractor_list.append((bool, _extract_bool))
         self._extractor_list.append((int, _extract_int))
@@ -23,6 +28,12 @@ class TypingExtractor(object):
         return True
 
     def extract(self, extractor, typ):
+        # PEP_560 deprecates issubclass for
+        # List types, for the time being
+        # we'll support a specific escape hatch.
+        if PEP_560:
+            if isinstance(typ, _GenericAlias) and typ.__origin__ is list:
+                return _extract_seq(extractor, typ)
         for t, t_extractor in self._extractor_list:
             if issubclass(typ, t):
                 return t_extractor(extractor, typ)
@@ -39,12 +50,14 @@ class TypingExtractor(object):
 def _extract_fallback(extractor, typ):
     return {"type": "object"}
 
+
 def _extract_seq(extractor, seq):
     """Convert a sequence to primitive equivalents."""
     subtype = Any
     if seq.__args__ and seq.__args__[0] is not Any:
         subtype = seq.__args__[0]
     return _array_type(extractor.extract(subtype))
+
 
 def _array_type(subtype_schema):
     return {
