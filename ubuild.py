12b46c5ba76efdeb36ae5d2ba673606d5d1943d2
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2020-08-12 08:46:18 -0700

    build: removing unneeded ubuild code
    removed old ubuild code, since we're now using uranium-plus

--- ubuild.py
+++ ubuild.py
@@ -1,8 +1,3 @@
-import os
-import uranium
-
-
-# ubuild.py
 build.packages.install("uranium-plus")
 import uranium_plus
 
@@ -13,47 +8,3 @@ build.config.update({
 })
 
 uranium_plus.bootstrap(build)
-
-
-# def main(build):
-#     build.packages.install(".", develop=True)
-#
-#
-# @uranium.task_requires("main")
-# def test(build):
-#     """ execute the unit tests. """
-#     build.packages.install("pytest")
-#     build.packages.install("pytest-benchmark")
-#     build.packages.install("pytest-cov")
-#     build.packages.install("flake8")
-#     build.packages.install("cattrs")
-#     build.executables.run([
-#         "py.test", "--cov", "jsonschema_extractor",
-#         "jsonschema_extractor/tests",
-#         "--cov-report", "term-missing"
-#     ] + build.options.args)
-#
-#
-# def publish(build):
-#     """ publish the package itself """
-#     build.packages.install("wheel")
-#     build.packages.install("twine")
-#     build.executables.run([
-#         "python", "setup.py",
-#         "sdist", "bdist_wheel", "--universal", "--release"
-#     ])
-#     build.executables.run([
-#         "twine", "upload", "dist/*"
-#     ])
-#
-#
-# def build_docs(build):
-#     build.packages.install("Babel")
-#     build.packages.install("Sphinx")
-#     build.packages.install("sphinx_rtd_theme")
-#     build.packages.install("sphinxcontrib-programoutput")
-#     return build.executables.run([
-#         "sphinx-build", "docs",
-#         os.path.join("docs", "_build")
-#     ] + build.options.args)[0]
-#
