57f28209b65f70fe666dd9c9dd58d17dca4cfd4f
Nir Geller <gnir.work@gmail.com>
2020-12-13 19:37:12 +0200

    Revert "Small changes to readme."
    This reverts commit 2898cdfb4f2352d36c10fd8acfe8d11351bce9d6.

--- jsonschema_extractor/__init__.py
+++ jsonschema_extractor/__init__.py
@@ -1,21 +1,17 @@
 from .extractor_set import SchemaExtractorSet
 
 from .typing_extractor import TypingExtractor
-
 DEFAULT_EXTRACTOR_LIST = [
     TypingExtractor()
 ]
 try:
     from .attrs_extractor import AttrsExtractor
-
     DEFAULT_EXTRACTOR_LIST.insert(0, AttrsExtractor())
-except ImportError:  # pragma: no cover
-    pass  # pragma: no cover
-
+except ImportError: # pragma: no cover
+    pass # pragma: no cover
 
 def extract_jsonschema(typ):
     return DEFAULT_EXTRACTOR.extract(typ)
-
 
 def init_default_extractor():
     """
@@ -24,10 +20,8 @@ def init_default_extractor():
     """
     return SchemaExtractorSet(DEFAULT_EXTRACTOR_LIST)
 
-
 DEFAULT_EXTRACTOR = init_default_extractor()
 from .exceptions import UnextractableSchema
-
 
 def extract(cls):
     return DEFAULT_EXTRACTOR.extract(cls)
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if "--release" in sys.argv:
 
 base = os.path.dirname(os.path.abspath(__file__))
 
-README_PATH = os.path.join(base, "README.md")
+README_PATH = os.path.join(base, "README.rst")
 
 install_requires = []
 
