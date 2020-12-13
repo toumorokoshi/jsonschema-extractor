1295d69fb2f5ca295fd1c4e481eddc83150a90de
Nir Geller <gnir.work@gmail.com>
2020-12-13 19:30:38 +0200

    Added test for attribute without type but with InValidator.

--- jsonschema_extractor/tests/test_attrs_validators.py
+++ jsonschema_extractor/tests/test_attrs_validators.py
@@ -9,6 +9,11 @@ class Example(object):
     options = attr.ib(type=str, validator=attr.validators.in_(POSSIBLE_OPTIONS))
 
 
+@attr.s
+class ExampleWithoutType(object):
+    options = attr.ib(validator=attr.validators.in_(POSSIBLE_OPTIONS))
+
+
 def test_in_validator_happy_flow(extractor):
     expected_schema = {
         'properties': {
@@ -23,3 +28,18 @@ def test_in_validator_happy_flow(extractor):
     }
 
     assert expected_schema == extractor.extract(Example)
+
+
+def test_in_validator_without_type(extractor):
+    expected_schema = {
+        'properties': {
+            'options': {
+                'enum': POSSIBLE_OPTIONS
+            }
+        },
+        'required': ['options'],
+        'title': 'ExampleWithoutType',
+        'type': 'object'
+    }
+
+    assert expected_schema == extractor.extract(ExampleWithoutType)
