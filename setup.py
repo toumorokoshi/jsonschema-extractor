5643b8667f99f780ed07d35896c7a5e20d780ad1
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2021-01-28 22:11:55 -0800

    minor: switching to setuptools-scm
    vcver is a deprecated package, so moving to the
    nearly equivalent setuptools-scm.

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-is_release = False
-if "--release" in sys.argv:
-    is_release = True
-    sys.argv.remove("--release")
-
 base = os.path.dirname(os.path.abspath(__file__))
 
 README_PATH = os.path.join(base, "README.rst")
@@ -22,34 +17,31 @@ if sys.version_info < (3, 5):
 
 tests_require = []
 
-setup(name='jsonschema-extractor',
-      setup_requires=["vcver"],
-      vcver={
-          "is_release": is_release,
-          "path": base
-      },
-      description=(
-          "a framework to extract jsonschema's from a variety of models."
-      ),
-      long_description=open(README_PATH).read(),
-      author='Yusuke Tsutsumi',
-      author_email='yusuke@tsutsumi.io',
-      url='https://github.com/toumorokoshi/attrs-jsonschema',
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=install_requires,
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Operating System :: MacOS',
-          'Operating System :: POSIX :: Linux',
-          'Topic :: System :: Software Distribution',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-      ],
-      tests_require=tests_require
-      )
+setup(
+    name="jsonschema-extractor",
+    use_scm_version={
+        "root": "..",
+        "relative_to": __file__,
+    },
+    setup_requires=["setuptools_scm"],
+    description=("a framework to extract jsonschema's from a variety of models."),
+    long_description=open(README_PATH).read(),
+    author="Yusuke Tsutsumi",
+    author_email="yusuke@tsutsumi.io",
+    url="https://github.com/toumorokoshi/attrs-jsonschema",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: System :: Software Distribution",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    tests_require=tests_require,
+)
